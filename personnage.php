5bb0458b5e87a6cf715ee7b3815408c1f84de2b2
ducdeswin7 <ducdeswin@outlook.com>
2014-12-18 19:15:43 +0100

    up2

--- personnage.php
+++ personnage.php
@@ -17,20 +17,20 @@
 	<hr>
 		<article class="rang1"> <!-- ligne numero 1 des perso -->
 			<ul>
-				<li><a href=""><img src="img/personnages/icon-ironman.png" alt="Iron Man"><h3>iron Man</h3></a></li>
-				<li><a href=""><img src="img/personnages/icon-cap.png" alt="Captain America"><h3>captain america</h3></a></li>
-				<li><a href=""><img src="img/personnages/icon-hulk.png" alt="hulk"><h3>hulk</h3></a></li>
-				<li><a href=""><img src="img/personnages/icon-thor.png" alt="Thor"><h3>thor</h3></a></li>
+				<li><a href="#iron"><img src="img/personnages/icon-ironman.png" alt="Iron Man"><h3>iron Man</h3></a></li>
+				<li><a href="#captain"><img src="img/personnages/icon-cap.png" alt="Captain America"><h3>captain america</h3></a></li>
+				<li><a href="#hulk"><img src="img/personnages/icon-hulk.png" alt="hulk"><h3>hulk</h3></a></li>
+				<li><a href="#thor"><img src="img/personnages/icon-thor.png" alt="Thor"><h3>thor</h3></a></li>
 			</ul>
 		</article> <!-- fin de la ligne --> 
 		<article class="rang2"> <!-- ligne numero 2 des perso -->
 			<ul>
-				<li><a href=""><img src="img/personnages/icon-fury.png" alt="Fury"><h3>fury</h3></a></li>
-				<li><a href=""><img src="img/personnages/icon-hawkeye.png" alt="Hawkeye"><h3>hawkeye</h3></a></li>
-				<li><a href=""><img src="img/personnages/icon-widow.png" alt="hulk"><h3>widow</h3></a></li>
-				<li><a href=""><img src="img/personnages/icon-witch.png" alt="Witch"><h3>witch</h3></a></li>
-				<li><a href=""><img src="img/personnages/icon-silver.png" alt="Quicksilver"><h3>quicksilver</h3></a></li>
-				<li><a href=""><img src="img/personnages/icon-ultron.png" alt="ultron"><h3>Ultron</h3></a></li>
+				<li><a href="#fury"><img src="img/personnages/icon-fury.png" alt="Fury"><h3>fury</h3></a></li>
+				<li><a href="#hawk"><img src="img/personnages/icon-hawkeye.png" alt="Hawkeye"><h3>hawkeye</h3></a></li>
+				<li><a href="#black"><img src="img/personnages/icon-widow.png" alt="hulk"><h3>widow</h3></a></li>
+				<li><a href="#scarlet"><img src="img/personnages/icon-witch.png" alt="Witch"><h3>witch</h3></a></li>
+				<li><a href="#vif"><img src="img/personnages/icon-silver.png" alt="Quicksilver"><h3>quicksilver</h3></a></li>
+				<li><a href="#ultron"><img src="img/personnages/icon-ultron.png" alt="ultron"><h3>Ultron</h3></a></li>
 			</ul>
 		</article> <!-- ligne numero 1 des perso -->
 	
